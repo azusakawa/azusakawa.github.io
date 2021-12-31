32d55f4dbfb85fae2c1927b416cf6cc59a70ec92
Azusakawa <azusakawayt@gmail.com>
2022-01-01 00:23:36 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -378,12 +378,6 @@ function GetInputFile(f) {
 
 function load_imag() {
     theDate = new Date();
-    $('<img />').attr('src','https://cctv7.kctmc.nat.gov.tw/play/jpg.php?devid={1bd7d468-24dd-6094-2d34-598be51a7948}&L=dfe8885c1acbe4c5e5fb91e6d9bd3724&'+theDate.getTime())   
-    .load(function(){ 
-        $('#live').empty();
-        $(this).width(340);
-        $(this).height(260);
-        $('#live').append( $(this) );
-    });
+    document.getElementById("live").src = 'https://cctv7.kctmc.nat.gov.tw/play/jpg.php?devid={1bd7d468-24dd-6094-2d34-598be51a7948}&L=dfe8885c1acbe4c5e5fb91e6d9bd3724&'+theDate.getTime()
     setTimeout("load_imag()",1000);
 }
