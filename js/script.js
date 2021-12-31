e2c6b8a791b9f95051bff84d63df7576d181b27a
Azusakawa <azusakawayt@gmail.com>
2022-01-01 00:30:15 +0800

    Main

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ var oddlot = '&oddLot=True'
 var quote = setInterval(Quotations, 1000)
 var chart = setInterval(Information, 1000)
 var pic = setInterval(StockChart, 60000)
-var load_imag = setInterval(load_imag(), 1000)
 
 function StockChart() {
     var apex = new ApexCharts(document.querySelector("#chart"), options);
