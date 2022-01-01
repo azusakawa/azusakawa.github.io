3ba09f33131f298dcf7db87c966fbf3a411d854c
Azusakawa <azusakawayt@gmail.com>
2022-01-01 22:27:08 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -376,7 +376,7 @@ function GetInputFile(f) {
     document.getElementById('ans').innerHTML = f.value
 }
 
-thisDate = new Date()
+var thisDate = new Date()
 var road_img = new Image()
 road_img.src = "http://cctv9.kctmc.nat.gov.tw/play/jpg.php?devid={e588681d-9714-bb94-31db-aa17b7e6f3ce}&L=33806a101aa5543ed05e71b6f76648a8" + thisDate.getTime()
 function load_imag() {
