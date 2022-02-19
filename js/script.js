afbaa8ffdb323117801ae401c33c662eae5f68b3
Azusakawa <azusakawayt@gmail.com>
2022-02-19 20:39:52 +0800

    main

--- js/script.js
+++ js/script.js
@@ -402,6 +402,12 @@ function ProfitTrial() {
             let price = document.getElementById('EnterBuyPrice').value
             let new_num = num * 1000
 
+            document.getElementById('t1').innerHTML = (res['bids'][0]['price'] * new_num)
+            document.getElementById('t2').innerHTML = Math.round(res['bids'][0]['price'] * new_num * 0.001425)
+            document.getElementById('t3').innerHTML = Math.round(res['bids'][0]['price'] * new_num * 0.0015)
+            document.getElementById('t4').innerHTML = (price * new_num)
+            document.getElementById('t5').innerHTML = Math.round(price * new_num * 0.001425)
+
             document.getElementById('sell_n1').innerHTML = res['bids'][0]['price']
             document.getElementById('sell_n2').innerHTML = res['bids'][1]['price']
             document.getElementById('sell_n3').innerHTML = res['bids'][2]['price']
@@ -414,7 +420,7 @@ function ProfitTrial() {
             document.getElementById('buy_n4').innerHTML = res['asks'][3]['price']
             document.getElementById('buy_n5').innerHTML = res['asks'][4]['price']
 
-            document.getElementById('sell_b1').innerHTML = (((res['bids'][0]['price'] * new_num) - (Math.round(res['bids'][0]['price'] * new_num * 0.001425)) - (Math.round(res['bids'][0]['price'] * new_num * 0.0015))) - ((price * new_num) + (Math.round(price * new_num * 0.001425))))
+            document.getElementById('sell_b1').innerHTML = res['bids'][0]['price']
             document.getElementById('sell_b2').innerHTML = res['bids'][1]['price']
             document.getElementById('sell_b3').innerHTML = res['bids'][2]['price']
             document.getElementById('sell_b4').innerHTML = res['bids'][3]['price']
